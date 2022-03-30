07896ba2c2e065dcbab6429792a8d565976a407a
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-03-30 21:27:32 +0600

    Config with Annotation. Component.

---
+++ src/main/java/spring_introduction/ConfigurationWithAnnotation.java
@@ -0,0 +1,15 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConfigurationWithAnnotation {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
+
+        Cat cat = context.getBean("cat", Cat.class);
+        cat.sayPet();
+
+        context.close();
+    }
+}
